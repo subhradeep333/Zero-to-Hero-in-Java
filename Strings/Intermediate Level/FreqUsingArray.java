f1e78e171591aac67869a453c11a4105b1390ee4
Namrata Kundu <namratakundu22work@gmail.com>
2026-04-24 02:01:59 +0530

    nk frequency print of letters using array added

---
+++ Strings/Intermediate Level/FreqUsingArray.java
@@ -0,0 +1,21 @@
+//Find the frequency of each character in a String using arrays
+import java.util.*;
+public class FreqUsingArray{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter String: ");
+        String words = sc.nextLine();
+        int[] freq = new int[256];
+        for(int i = 0; i<words.length(); i++){
+            freq[words.charAt(i)]++ ;
+        }
+        for(int i=0; i<words.length(); i++){
+            if(words.charAt(i) !=' ' && freq[words.charAt(i)]>0){
+                System.out.println(words.charAt(i) + "--> "+ freq[words.charAt(i)]);
+            }
+            freq[words.charAt(i)] = 0; 
+            //change the frequency of that letter to 0, if printed once --> distinct letters printed, no repetition 
+        }
+        sc.close();
+    }
+}
