4ce554cb3e19a7f05dacae8ddac684d7ac1296fa
Namrata Kundu <namratakundu22work@gmail.com>
2026-04-24 02:25:12 +0530

    nk folder structure update: folders renamed

---
+++ Strings/🟢Basic Level/FindSubString.java
@@ -0,0 +1,10 @@
+//Given a string s, you need to slice it so that the output is a substring that contains all the characters except the first and last.
+import java.util.*;
+public class FindSubString{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a string: ");
+        String s = sc.nextLine();
+        
+    }
+}
