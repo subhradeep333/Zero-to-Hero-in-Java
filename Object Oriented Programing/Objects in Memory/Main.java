fe7a668383ea98db2664c53e6fd4858acd62a7a4
Subhradeep333 <codewithsubhradeep3@gmail.com>
2026-07-11 11:10:28 -0700

    object in memory stored

---
+++ Object Oriented Programing/Objects in Memory/Main.java
@@ -0,0 +1,42 @@
+class Student {
+    String name;
+    int age;
+
+    Student(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+
+        Student s1 = new Student("Subhradeep", 20);
+        Student s2 = new Student("Rahul", 21);
+    }
+}
+
+// Stack
+
+// s1 --------+
+//            |
+// s2 -----+  |
+//          | |
+//          | |
+//          v v
+
+// Heap
+
+// +-------------------------+
+// | Student Object          |
+// |-------------------------|
+// | name = "Subhradeep"     |
+// | age  = 20               |
+// +-------------------------+
+
+// +-------------------------+
+// | Student Object          |
+// |-------------------------|
+// | name = "Rahul"          |
+// | age  = 21               |
+// +-------------------------+
